ba38bd35f9d85eda60d4700a6a83fefa20e09910
Maria Shestakova <marijashestakova73@gmail.com>
2019-04-12 18:15:27 +0400

    Add only necessary files

---
+++ api/main2.py
@@ -0,0 +1,200 @@
+import pygame
+import requests
+import sys
+import os
+from common.geocoder import geocode, get_coordinates
+from common.mapapi import show_map
+
+
+import math
+
+from common.distance import lonlat_distance
+from common.business import find_business
+
+# Подобранные констатны для поведения карты.
+LAT_STEP = 0.008  # Шаги при движении карты по широте и долготе
+LON_STEP = 0.02
+coord_to_geo_x = 0.0000428  # Пропорции пиксельных и географических координат.
+coord_to_geo_y = 0.0000428
+pygame.init()
+
+def ll(x, y):
+    return "{0},{1}".format(x, y)
+
+
+# Параметры отображения карты:
+# координаты, масштаб, найденные объекты и т.д.
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+
+
+class MapParams(object):
+    # Параметры по умолчанию.
+    def __init__(self):
+        self.lat = 55.729738  # Координаты центра карты на старте.
+        self.lon = 37.664777
+        self.zoom = 10  # Масштаб карты на старте.
+        self.type = "map"  # Тип карты на старте.
+
+        self.search_result = None  # Найденный объект для отображения на карте.
+        self.use_postal_code = False
+
+    # Преобразование координат в параметр ll
+    def ll(self):
+        return ll(self.lon, self.lat)
+
+    # Обновление параметров карты по нажатой клавише.
+    def update(self, event):
+
+        if event.key == pygame.K_UP:
+            self.lat += LON_STEP * 2 **(15 - self.zoom)
+        if event.key == pygame.K_DOWN:
+            self.lat -= LON_STEP * 2 **(15 - self.zoom)
+        if event.key == pygame.K_RIGHT:
+            self.lon += LAT_STEP * 2 **(15 - self.zoom)
+        if event.key == pygame.K_LEFT:
+            self.lon -= LAT_STEP * 2 **(15 - self.zoom)
+
+        if event.key == pygame.K_PAGEUP and self.zoom < 19:
+            self.zoom += 1
+
+        if event.key == pygame.K_PAGEDOWN and self.zoom > 0:
+            self.zoom -= 1
+
+        if event.key == pygame.K_1:
+            self.type = 'map'
+
+        if event.key == pygame.K_2:
+            self.type = 'sat'
+
+        if event.key == pygame.K_3:
+            self.type = 'sat,skl'
+
+
+
+    # Преобразование экранных координат в географические.
+    def screen_to_geo(self, pos):
+        dy = 225 - pos[1]
+        dx = pos[0] - 300
+        lx = self.lon + dx * coord_to_geo_x * math.pow(2, 15 - self.zoom)
+        ly = self.lat + dy * coord_to_geo_y * math.cos(math.radians(self.lat)) * math.pow(2, 15 - self.zoom)
+        return lx, ly
+
+    # еще несколько функций
+
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={0},{1}&z={2}&l={3}".format(mp.ll()[0], mp.ll()[1], mp.zoom, mp.type)
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        return("Ошибка записи временного файла:", ex)
+
+    return map_file
+
+
+
+def main():
+
+    screen = pygame.display.set_mode((600, 450))
+    input_box = InputBox(0, 0, 140, 32)
+
+    # Заводим объект, в котором будем хранить все параметры отрисовки карты.
+    mp = MapParams()
+
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:  # Выход из программы
+            break
+        elif event.type == pygame.KEYUP:  # Обрабатываем различные нажатые клавиши.
+            if event.key == pygame.K_4:
+                if input_box.text != '':
+                    lon, lat = get_coordinates(input_box.text)
+                    point = 'pt={},{}'.format(lon, lat)
+                    show_map(lon, lat, add_params=point)
+                    mp2 = MapParams()
+                    mp2.lat = lat
+                    mp2.lon = lon
+                    map_file2 = load_map(mp2)
+                    screen.blit(pygame.image.load(map_file2), (0, 0))
+                    mp2.update(event)
+
+            mp.update(event)
+
+            # другие eventы
+
+        input_box.handle_event(event)
+        input_box.update()
+
+        # Загружаем карту, используя текущие параметры.
+        map_file = load_map(mp)
+
+        # Рисуем картинку, загружаемую из только что созданного файла.
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        input_box.draw(screen)
+
+        # Переключаем экран и ждем закрытия окна.
+        pygame.display.flip()
+
+    pygame.quit()
+    # Удаляем за собой файл с изображением.
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
